e631e6c8cba036fea3e152c372ec9562cae5ffd8
Shourya <shourya124sensei@gmail.com>
2025-09-24 23:41:34 +0530

    Ansh added student-dashboard

--- src/components/QuickActions.jsx
+++ src/components/QuickActions.jsx
@@ -9,19 +9,19 @@ export default function QuickActions() {
         <div className="quick-actions-container">
             <h3>Quick Actions</h3>
             <div className="actions-grid">
-                <button className="action-button" onClick={()=>navigate("/genTT")}>
+                <button className="action-button" onClick={()=>navigate("/admin-genTT")}>
                     <PlusCircle size={24} />
                     <span>Generate New Timetable</span>
                 </button>
-                <button className="action-button" onClick={()=>navigate("/timetable")}>
+                <button className="action-button" onClick={()=>navigate("/admin-timetable")}>
                     <CalendarDays size={24} />
                     <span>View All Timetables</span>
                 </button>
-                <button className="action-button" onClick={()=>navigate("/faculty")}>
+                <button className="action-button" onClick={()=>navigate("/admin-faculty")}>
                     <Users size={24} />
                     <span>Add New Faculty</span>
                 </button>
-                <button className="action-button" onClick={()=>navigate("/rooms")}>
+                <button className="action-button" onClick={()=>navigate("/admin-rooms")}>
                     <School size={24} />
                     <span>Add New Classroom</span>
                 </button>
